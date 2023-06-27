f2de009d8c61482cf4beb9a7e6e4393e468d8d7a
Roberto Arosemena <robert@lotus.mx>
2023-06-27 09:34:37 -0600

    Fixed prettier check

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -26,7 +26,7 @@ lsp.configure('eslint', {
 lsp.configure('tsserver', {
   on_attach = function(_, bufnr)
     local hasPrettier = vim.fn.filereadable('.prettierrc')
-    if hasPrettier then
+    if hasPrettier ~= 0 then
       vim.api.nvim_create_autocmd('BufWritePre', {
         buffer = bufnr,
         callback = function()
--- .config/nvim/lua/arosemena/remap.lua
+++ .config/nvim/lua/arosemena/remap.lua
@@ -15,6 +15,8 @@ vim.keymap.set('n', '<C-u>', '<C-u>zz')
 
 vim.keymap.set('v', '<leader>y', '"*y', {}) -- yank to system clipboard
 
+vim.keymap.set('n', '<leader>w', ':w<CR>')
+
 -- Close quickfix window with <CR>
 vim.api.nvim_create_autocmd(
   'FileType', {
